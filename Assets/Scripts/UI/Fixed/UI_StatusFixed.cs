d71ffcdece0d9e9b04b17cbdd8e3afac880271ad
DaeHan Jang <abouttt57@gmail.com>
2023-11-22 19:05:22 +0900

    플레이어 스테이터스 UI 추가

---
+++ Assets/Scripts/UI/Fixed/UI_StatusFixed.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class UI_StatusFixed : UI_Base
+{
+    enum Images
+    {
+        HPBar,
+        MPBar,
+        SPBar,
+        XPBar,
+    }
+
+    enum Texts
+    {
+        LevelText,
+    }
+
+    protected override void Init()
+    {
+        BindImage(typeof(Images));
+        BindText(typeof(Texts));
+
+        Player.Status.LevelChanged += RefreshLevelText;
+        Player.Status.HPChanged += RefreshHPImage;
+        Player.Status.MPChanged += RefreshMPImage;
+        Player.Status.SPChanged += RefreshSPImage;
+        Player.Status.XPChanged += RefreshXPImage;
+        Player.Status.StatChanged += RefreshAllStatusImage;
+    }
+
+    private void Start()
+    {
+        Managers.UI.Register<UI_StatusFixed>(this);
+        RefreshAllStatusImage();
+    }
+
+    private void RefreshAllStatusImage()
+    {
+        RefreshLevelText();
+        RefreshHPImage();
+        RefreshMPImage();
+        RefreshSPImage();
+        RefreshXPImage();
+    }
+
+    private void RefreshLevelText() => GetText((int)Texts.LevelText).text = $"Lev.{Player.Status.Level}";
+    private void RefreshHPImage() => GetImage((int)Images.HPBar).fillAmount = (float)Player.Status.HP / Player.Status.MaxStat.HP;
+    private void RefreshMPImage() => GetImage((int)Images.MPBar).fillAmount = (float)Player.Status.MP / Player.Status.MaxStat.MP;
+    private void RefreshSPImage() => GetImage((int)Images.SPBar).fillAmount = (float)Player.Status.SP / Player.Status.MaxStat.SP;
+    private void RefreshXPImage() => GetImage((int)Images.XPBar).fillAmount = (float)Player.Status.XP / Player.Status.MaxStat.XP;
+}
