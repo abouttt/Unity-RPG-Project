508a809d6ad7fa74f83d4971e3c193fc23e2a9f1
DaeHan Jang <abouttt57@gmail.com>
2023-12-12 21:35:57 +0900

    미니맵에 마우스 레이를 통한 물체 감별 기능 추

--- Assets/Scripts/UI/Fixed/UI_MinimapFixed.cs
+++ Assets/Scripts/UI/Fixed/UI_MinimapFixed.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class UI_MinimapFixed : UI_Base
+public class UI_MinimapFixed : UI_Base, IPointerMoveHandler
 {
+    enum RawImages
+    {
+        MinimapImage,
+    }
+
     [SerializeField]
     private float _height;
 
-    private GameObject _minimapCamera;
+    private Camera _minimapCamera;
 
     protected override void Init()
     {
-        _minimapCamera = GameObject.FindWithTag("MinimapCamera");
+        Bind<RawImage>(typeof(RawImages));
+        _minimapCamera = GameObject.FindWithTag("MinimapCamera").GetComponent<Camera>();
     }
 
     private void Start()
@@ -25,5 +33,35 @@ public class UI_MinimapFixed : UI_Base
         euler.x = 90f;
         euler.z = 0f;
         _minimapCamera.transform.SetPositionAndRotation(position, Quaternion.Euler(euler));
+    }
+
+    public void OnPointerMove(PointerEventData eventData)
+    {
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(Get<RawImage>((int)RawImages.MinimapImage).rectTransform,
+                eventData.position, eventData.enterEventCamera, out var cursor))
+        {
+            Texture texture = Get<RawImage>((int)RawImages.MinimapImage).texture;
+            Rect rect = Get<RawImage>((int)RawImages.MinimapImage).rectTransform.rect;
+
+            float coordX = Mathf.Clamp(0, (((cursor.x - rect.x) * texture.width) / rect.width), texture.width);
+            float coordY = Mathf.Clamp(0, (((cursor.y - rect.y) * texture.height) / rect.height), texture.height);
+
+            float calX = coordX / texture.width;
+            float calY = coordY / texture.height;
+
+            cursor = new Vector2(calX, calY);
+
+            CastRayToWorld(cursor);
+        }
+    }
+
+    private void CastRayToWorld(Vector2 vec)
+    {
+        int layerMask = 1 << LayerMask.NameToLayer("Minimap");
+        Ray mapRay = _minimapCamera.ScreenPointToRay(new Vector2(vec.x * _minimapCamera.pixelWidth, vec.y * _minimapCamera.pixelHeight));
+        if (Physics.Raycast(mapRay, out var miniMapHit, Mathf.Infinity, layerMask))
+        {
+            Debug.Log("miniMapHit: " + miniMapHit.collider.gameObject);
+        }
     }
 }
