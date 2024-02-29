02ae93cf7ebfa15f23d29765b3195f033b95bf6f
DaeHan Jang <abouttt57@gmail.com>
2024-02-29 15:35:30 +0900

    GoblinThrowerThrowingWeapon.cs -> MonsterThrowingWeapon 클래스명 변경

--- Assets/Scripts/Monster/MonsterThrowingWeapon.cs
+++ Assets/Scripts/Monster/MonsterThrowingWeapon.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class GoblinThrowerThrowingWeapon : MonoBehaviour
+public class MonsterThrowingWeapon : MonoBehaviour
 {
     [SerializeField]
     private MonsterData _goblinThrowerData;
