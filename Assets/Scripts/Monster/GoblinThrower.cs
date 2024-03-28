8434581ef388a7b431675ff0608664b0e5027d0b
DaeHan Jang <abouttt57@gmail.com>
2024-03-28 12:47:15 +0900

    고블린 투척병 플레이어가 가까이 있으며 구르기시에는 투척물을 던짐

--- Assets/Scripts/Monster/GoblinThrower.cs
+++ Assets/Scripts/Monster/GoblinThrower.cs
@@ -18,15 +18,17 @@ public class GoblinThrower : Monster
         int cnt = Physics.OverlapSphereNonAlloc(AttackOffset.position, AttackRadius, PlayerCollider, 1 << LayerMask.NameToLayer("Player"));
         if (cnt != 0)
         {
-            Player.Battle.TakeDamage(this, transform.position, Data.Damage, true);
+            if (!Player.Movement.IsRolling)
+            {
+                Player.Battle.TakeDamage(this, transform.position, Data.Damage, true);
+                return;
+            }
         }
-        else
-        {
-            var weapon = Managers.Resource.Instantiate(
-            "GoblinThrowerThrowingWeapon.prefab", _rightHand.position, transform.rotation * Quaternion.Euler(90f, 0f, 0f), null, true);
-            var projectile = weapon.GetComponent<Projectile>();
-            projectile.Damage = Data.Damage;
-            projectile.Shoot(transform.forward * _projectileSpeed);
-        }
+
+        var weapon = Managers.Resource.Instantiate(
+        "GoblinThrowerThrowingWeapon.prefab", _rightHand.position, transform.rotation * Quaternion.Euler(90f, 0f, 0f), null, true);
+        var projectile = weapon.GetComponent<Projectile>();
+        projectile.Damage = Data.Damage;
+        projectile.Shoot(transform.forward * _projectileSpeed);
     }
 }
